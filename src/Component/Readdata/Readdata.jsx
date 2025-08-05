43f50301cd8b151024a169e3f1fd7789550817b9
RHrakib1 <rakibhasanmd457@gmail.com>
2025-08-06 00:23:38 +0600

    add

--- src/Component/Readdata/Readdata.jsx
+++ src/Component/Readdata/Readdata.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+
 import { useLoaderData } from 'react-router-dom'
 
 export default function Readdata() {
     const dataload = useLoaderData()
+    const hendleclickDelete = id => {
+        console.log('delete id', id)
+        fetch(`http://localhost:5000/datauser/${id}`, {
+            method: 'DELETE',
+        })
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }
+
     return (
         <div>
             <h1>data: {dataload.length}</h1>
             {
-                dataload.map(d => <p>{d.name}: {d.email}</p>)
+                dataload.map(d => <p>{d.name}: {d.email} ---{d._id} <button onClick={() => hendleclickDelete(d._id)}>X</button></p>)
             }
         </div>
     )
